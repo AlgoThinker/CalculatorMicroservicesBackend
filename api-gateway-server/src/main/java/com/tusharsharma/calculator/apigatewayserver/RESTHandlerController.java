052f38f444401c2d3dda76121e0785da713cf1e1
Tushar Sharma <tushar123cse@gmail.com>
2019-11-11 09:41:33 +0530

    More code

--- api-gateway-server/src/main/java/com/tusharsharma/calculator/apigatewayserver/RESTHandlerController.java
+++ api-gateway-server/src/main/java/com/tusharsharma/calculator/apigatewayserver/RESTHandlerController.java
@@ -38,11 +38,11 @@ public class RESTHandlerController {
 	@PostMapping("api-gateway")
 	String performOperation(@RequestBody String expression) {
 	
-		return Integer.toString(RESTHandlerController.evaluate(expression));
+		return Double.toString(RESTHandlerController.evaluate(expression));
 
 	}
 
-	public static int evaluate(String exp) {
+	public static Double evaluate(String exp) {
 
 		String expSpacePlus = exp.replaceAll("\\+", " \\+ ");
 		String expSpaceMinus = expSpacePlus.replaceAll("-", " - ");
@@ -50,11 +50,13 @@ public class RESTHandlerController {
 		String expSpaceMulti = expSpaceDivision.replaceAll("\\*", " \\* ");
 		String expSpaceBracket = expSpaceMulti.replaceAll("\\(", " \\( ");
 		String expression = expSpaceBracket.replaceAll("\\)", " \\) ");
+		
+		System.out.print(expression);
 
 		char[] tokens = expression.toCharArray();
 
 		// Stack for numbers: 'values'
-		Stack<Integer> values = new Stack<Integer>();
+		Stack<Double> values = new Stack<Double>();
 
 		// Stack for Operators: 'ops'
 		Stack<Character> ops = new Stack<Character>();
@@ -65,12 +67,12 @@ public class RESTHandlerController {
 				continue;
 
 			// Current token is a number, push it to stack for numbers
-			if (tokens[i] >= '0' && tokens[i] <= '9') {
+			if (tokens[i] >= '0' && tokens[i] <= '9' || tokens[i] == '.') {
 				StringBuffer sbuf = new StringBuffer();
 				// There may be more than one digits in number
-				while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
+				while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9' || tokens[i] == '.')
 					sbuf.append(tokens[i++]);
-				values.push(Integer.parseInt(sbuf.toString()));
+				values.push(Double.parseDouble(sbuf.toString()));
 			}
 
 			// Current token is an opening brace, push it to 'ops'
@@ -119,7 +121,7 @@ public class RESTHandlerController {
 
 	// A utility method to apply an operator 'op' on operands 'a'
 	// and 'b'. Return the result.
-	public static int applyOp(char op, int b, int a) {
+	public static Double applyOp(char op, Double b, Double a) {
 		switch (op) {
 		case '+':
 			return a + b;
@@ -132,7 +134,105 @@ public class RESTHandlerController {
 				throw new UnsupportedOperationException("Cannot divide by zero");
 			return a / b;
 		}
-		return 0;
+		return 0.0;
 	}
+	
+	
+	
+	
+	
+//	
+//	public static int evaluate(String exp) {
+//
+//		String expSpacePlus = exp.replaceAll("\\+", " \\+ ");
+//		String expSpaceMinus = expSpacePlus.replaceAll("-", " - ");
+//		String expSpaceDivision = expSpaceMinus.replaceAll("/", " / ");
+//		String expSpaceMulti = expSpaceDivision.replaceAll("\\*", " \\* ");
+//		String expSpaceBracket = expSpaceMulti.replaceAll("\\(", " \\( ");
+//		String expression = expSpaceBracket.replaceAll("\\)", " \\) ");
+//
+//		char[] tokens = expression.toCharArray();
+//
+//		// Stack for numbers: 'values'
+//		Stack<Integer> values = new Stack<Integer>();
+//
+//		// Stack for Operators: 'ops'
+//		Stack<Character> ops = new Stack<Character>();
+//
+//		for (int i = 0; i < tokens.length; i++) {
+//			// Current token is a whitespace, skip it
+//			if (tokens[i] == ' ')
+//				continue;
+//
+//			// Current token is a number, push it to stack for numbers
+//			if (tokens[i] >= '0' && tokens[i] <= '9') {
+//				StringBuffer sbuf = new StringBuffer();
+//				// There may be more than one digits in number
+//				while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
+//					sbuf.append(tokens[i++]);
+//				values.push(Integer.parseInt(sbuf.toString()));
+//			}
+//
+//			// Current token is an opening brace, push it to 'ops'
+//			else if (tokens[i] == '(')
+//				ops.push(tokens[i]);
+//
+//			// Closing brace encountered, solve entire brace
+//			else if (tokens[i] == ')') {
+//				while (ops.peek() != '(')
+//					values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+//				ops.pop();
+//			}
+//
+//			// Current token is an operator.
+//			else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
+//				// While top of 'ops' has same or greater precedence to current
+//				// token, which is an operator. Apply operator on top of 'ops'
+//				// to top two elements in values stack
+//				while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+//					values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+//
+//				// Push current token to 'ops'.
+//				ops.push(tokens[i]);
+//			}
+//		}
+//
+//		// Entire expression has been parsed at this point, apply remaining
+//		// ops to remaining values
+//		while (!ops.empty())
+//			values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+//
+//		// Top of 'values' contains result, return it
+//		return values.pop();
+//	}
+//
+//	// Returns true if 'op2' has higher or same precedence as 'op1',
+//	// otherwise returns false.
+//	public static boolean hasPrecedence(char op1, char op2) {
+//		if (op2 == '(' || op2 == ')')
+//			return false;
+//		if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+//			return false;
+//		else
+//			return true;
+//	}
+//
+//	// A utility method to apply an operator 'op' on operands 'a'
+//	// and 'b'. Return the result.
+//	public static int applyOp(char op, int b, int a) {
+//		switch (op) {
+//		case '+':
+//			return a + b;
+//		case '-':
+//			return a - b;
+//		case '*':
+//			return a * b;
+//		case '/':
+//			if (b == 0)
+//				throw new UnsupportedOperationException("Cannot divide by zero");
+//			return a / b;
+//		}
+//		return 0;
+//	}
 
 }
