ac5eb9b864988af6aef4064de0a78b07271a5e7a
Tushar Sharma <tushar123cse@gmail.com>
2019-11-12 00:32:15 +0530

    optimization

--- api-gateway-server/src/main/java/com/tusharsharma/calculator/apigatewayserver/ApiGatewayServerApplication.java
+++ api-gateway-server/src/main/java/com/tusharsharma/calculator/apigatewayserver/ApiGatewayServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
 @EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,5 +16,5 @@ public class ApiGatewayServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayServerApplication.class, args);
 	}
-
+	
 }
--- api-gateway-server/src/main/java/com/tusharsharma/calculator/apigatewayserver/RESTHandlerController.java
+++ api-gateway-server/src/main/java/com/tusharsharma/calculator/apigatewayserver/RESTHandlerController.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +35,9 @@ public class RESTHandlerController {
 	private DivisionMicroserviceProxy divisionProxy;
 	@Autowired
 	private PercentageMicroserviceProxy percentageProxy;
-
+	
+	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("api-gateway")
 	String performOperation(@RequestBody String expression) {
 	
@@ -51,16 +54,8 @@ public class RESTHandlerController {
 	}
 
 	public Double evaluate(String exp) {
-
-		String expSpacePlus = exp.replaceAll("\\+", " \\+ ");
-		String expSpaceMinus = expSpacePlus.replaceAll("-", " - ");
-		String expSpaceDivision = expSpaceMinus.replaceAll("/", " / ");
-		String expSpaceMulti = expSpaceDivision.replaceAll("\\*", " \\* ");
-		String expSpaceBracket = expSpaceMulti.replaceAll("\\(", " \\( ");
-		String expMayBewithBrackets = expSpaceBracket.replaceAll("\\)", " \\) ");
-		String expression = " ( "+expMayBewithBrackets+" ) ";
-		
-		System.out.print(expression);
+	
+		String expression = preprocessString(exp);
 
 		char[] tokens = expression.toCharArray();
 
@@ -117,6 +112,7 @@ public class RESTHandlerController {
 		return values.pop();
 	}
 
+
 	// Returns true if 'op2' has higher or same precedence as 'op1',
 	// otherwise returns false.
 	public static boolean hasPrecedence(char op1, char op2) {
@@ -150,102 +146,16 @@ public class RESTHandlerController {
 		return 0.0;
 	}
 	
-	
-	
-	
-	
-//	
-//	public static int evaluate(String exp) {
-//
-//		String expSpacePlus = exp.replaceAll("\\+", " \\+ ");
-//		String expSpaceMinus = expSpacePlus.replaceAll("-", " - ");
-//		String expSpaceDivision = expSpaceMinus.replaceAll("/", " / ");
-//		String expSpaceMulti = expSpaceDivision.replaceAll("\\*", " \\* ");
-//		String expSpaceBracket = expSpaceMulti.replaceAll("\\(", " \\( ");
-//		String expression = expSpaceBracket.replaceAll("\\)", " \\) ");
-//
-//		char[] tokens = expression.toCharArray();
-//
-//		// Stack for numbers: 'values'
-//		Stack<Integer> values = new Stack<Integer>();
-//
-//		// Stack for Operators: 'ops'
-//		Stack<Character> ops = new Stack<Character>();
-//
-//		for (int i = 0; i < tokens.length; i++) {
-//			// Current token is a whitespace, skip it
-//			if (tokens[i] == ' ')
-//				continue;
-//
-//			// Current token is a number, push it to stack for numbers
-//			if (tokens[i] >= '0' && tokens[i] <= '9') {
-//				StringBuffer sbuf = new StringBuffer();
-//				// There may be more than one digits in number
-//				while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
-//					sbuf.append(tokens[i++]);
-//				values.push(Integer.parseInt(sbuf.toString()));
-//			}
-//
-//			// Current token is an opening brace, push it to 'ops'
-//			else if (tokens[i] == '(')
-//				ops.push(tokens[i]);
-//
-//			// Closing brace encountered, solve entire brace
-//			else if (tokens[i] == ')') {
-//				while (ops.peek() != '(')
-//					values.push(applyOp(ops.pop(), values.pop(), values.pop()));
-//				ops.pop();
-//			}
-//
-//			// Current token is an operator.
-//			else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
-//				// While top of 'ops' has same or greater precedence to current
-//				// token, which is an operator. Apply operator on top of 'ops'
-//				// to top two elements in values stack
-//				while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
-//					values.push(applyOp(ops.pop(), values.pop(), values.pop()));
-//
-//				// Push current token to 'ops'.
-//				ops.push(tokens[i]);
-//			}
-//		}
-//
-//		// Entire expression has been parsed at this point, apply remaining
-//		// ops to remaining values
-//		while (!ops.empty())
-//			values.push(applyOp(ops.pop(), values.pop(), values.pop()));
-//
-//		// Top of 'values' contains result, return it
-//		return values.pop();
-//	}
-//
-//	// Returns true if 'op2' has higher or same precedence as 'op1',
-//	// otherwise returns false.
-//	public static boolean hasPrecedence(char op1, char op2) {
-//		if (op2 == '(' || op2 == ')')
-//			return false;
-//		if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
-//			return false;
-//		else
-//			return true;
-//	}
-//
-//	// A utility method to apply an operator 'op' on operands 'a'
-//	// and 'b'. Return the result.
-//	public static int applyOp(char op, int b, int a) {
-//		switch (op) {
-//		case '+':
-//			return a + b;
-//		case '-':
-//			return a - b;
-//		case '*':
-//			return a * b;
-//		case '/':
-//			if (b == 0)
-//				throw new UnsupportedOperationException("Cannot divide by zero");
-//			return a / b;
-//		}
-//		return 0;
-//	}
+	public String preprocessString(String exp){
+		
+		String expSpacePlus = exp.replaceAll("\\+", " \\+ ");
+		String expSpaceMinus = expSpacePlus.replaceAll("-", " - ");
+		String expSpaceDivision = expSpaceMinus.replaceAll("/", " / ");
+		String expSpaceMulti = expSpaceDivision.replaceAll("\\*", " \\* ");
+		String expSpaceBracket = expSpaceMulti.replaceAll("\\(", " \\( ");
+		String expMayBewithBrackets = expSpaceBracket.replaceAll("\\)", " \\) ");
+		String expression = " ( "+expMayBewithBrackets+" ) ";
+		return expression;
+	}
 
 }
